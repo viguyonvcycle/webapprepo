a5f4af4f8a471599c116fd45fdce39eefc6338a0
viguyonvcycle <viguyonv@cyclesupervision.onmicrosoft.com>
2020-12-29 10:35:26 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer(function(request, response){
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.end("<html><body><h1>Bonjour bonjour !</h1></body></html>")
+});
+
+const port = process.env.PORT || 1337; 
+server.listen(port);
+
+console.log(`Server running at http://localhost:{$port}`);
+
